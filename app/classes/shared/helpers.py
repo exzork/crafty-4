exzork/crafty-4

commit 102f63c771d9e3d4d330750dabae4a749ec9f472

Move session.lock, Database And config.json To Config Folder

app/classes/shared/helpers.py
@@ -36,10 +36,10 @@ class Helpers:
         self.webroot = os.path.join(self.root_dir, 'app', 'frontend')
         self.servers_dir = os.path.join(self.root_dir, 'servers')
 
-        self.session_file = os.path.join(self.root_dir, 'session.lock')
-        self.settings_file = os.path.join(self.root_dir, 'config.json')
+        self.session_file = os.path.join(self.root_dir, 'app', 'config', 'session.lock')
+        self.settings_file = os.path.join(self.root_dir, 'app', 'config', 'config.json')
 
-        self.db_path = os.path.join(self.root_dir, 'crafty.sqlite')
+        self.db_path = os.path.join(self.root_dir, 'app', 'config', 'db', 'crafty.sqlite')
         self.serverjar_cache = os.path.join(self.config_dir, 'serverjars.json')
         self.passhasher = PasswordHasher()
         self.exiting = False

commit 1542f2559a045ff850efc2c8881294744f754b5b

adding creation of app/config/db directory to helper so if it's not there,it will be created bug introduced by commit 102f63c7

app/classes/shared/helpers.py
@@ -39,6 +39,7 @@ class Helpers:
         self.session_file = os.path.join(self.root_dir, 'app', 'config', 'session.lock')
         self.settings_file = os.path.join(self.root_dir, 'app', 'config', 'config.json')
 
+        self.ensure_dir_exists(os.path.join(self.root_dir, 'app', 'config', 'db'))
         self.db_path = os.path.join(self.root_dir, 'app', 'config', 'db', 'crafty.sqlite')
         self.serverjar_cache = os.path.join(self.config_dir, 'serverjars.json')
         self.passhasher = PasswordHasher()