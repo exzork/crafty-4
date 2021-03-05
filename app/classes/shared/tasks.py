06a5f4713e57d629e61e42832c31144d17d9689d
luukas <lukasdoesdev@gmail.com>
2021-03-05 11:30:18 +0200

    fix merge conflict in tasks.py

--- app/classes/shared/tasks.py
+++ app/classes/shared/tasks.py
@@ -167,6 +167,11 @@ class TasksManager:
             else:
                 # Stats are same
                 time.sleep(8)
+    def log_watcher(self):
+        console.debug('in log_watcher')
+        helper.check_for_old_logs(db_helper)
+        schedule.every(6).hours.do(lambda: helper.check_for_old_logs(db_helper))
+
 
 
 
