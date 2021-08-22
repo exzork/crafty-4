000c764ca3e324edc23603cf24ce50bdb126e138
Andrew <mcdeweykp@gmail.com>
2021-08-22 17:40:28 -0400

    Fix bandaid on migrations...let's call it a cast

--- app/migrations/20210819155737_permissions.py
+++ app/migrations/20210819155737_permissions.py
@@ -2,8 +2,8 @@
 import peewee
 
 def migrate(migrator, database, **kwargs):
-    migrator.drop_columns('user_servers', permissions=peewee.CharField(default="00000000")) # First argument can be model class OR table name
-    migrator.add_columns('role_servers', permissions=peewee.CharField(default="00000000")) # First argument can be model class OR table name
+    migrator.drop_columns('user_servers', ['permissions']) # First argument can be model class OR table name
+    #migrator.add_columns('role_servers', permissions=peewee.CharField(default="00000000")) # First argument can be model class OR table name
     """
     Write your migrations here.
     """
