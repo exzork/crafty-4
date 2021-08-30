690669e1da8ee5da58c1e2566ac0b5630c7ad2e6
Zedifus <zedifus@zediverse.gg>
2021-08-30 03:48:36 +0100

    Fix directory creation issue with docker run

--- docker_launcher.sh
+++ docker_launcher.sh
@@ -2,6 +2,7 @@
 
 # Check if config exists from existing installation (venv or previous docker launch)
 if [ ! "$(ls -A ./app/config)" ]; then
+    mkdir ./app/config/
     cp -r ./app/config_original/* ./app/config/
 fi
 
