57bdeac2c2a32a23b626a4c06c0be738850e93b4
Zedifus <zedifus@zediverse.gg>
2022-03-07 01:51:25 +0000

    Use sudo to drop perms to non-root

--- docker_launcher.sh
+++ docker_launcher.sh
@@ -2,10 +2,32 @@
 
 # Check if config exists from existing installation (venv or previous docker launch)
 if [ ! "$(ls -A --ignore=.gitkeep ./app/config)" ]; then
-    mkdir ./app/config/
+    echo "Wrapper | Config not found, pulling defaults..."
+    mkdir ./app/config/ 2> /dev/null
     cp -r ./app/config_original/* ./app/config/
 fi
 
-# Activate our prepared venv and launch crafty with provided args
-. .venv/bin/activate
-exec python3 main.py $@
+
+if [ $(id -u) -eq 0 ]; then
+    # We're running as root;
+    # Need to ensure all dirs are owned by the root group,
+    # This fixes bind mounts that may have incorrect perms.
+
+    # Look for files & dirs that require group permissions to be fixed
+    echo "Wrapper | Looking for problem bind mount permissions"
+    find . ! -group root -exec chgrp root {} \;
+    find . ! -perm g+rw -exec chmod g+rw {} \;
+    find . -type d ! -perm g+s -exec chmod g+s {} \;
+
+    # Switch user, activate our prepared venv and lauch crafty
+    args="$@"
+    echo "Wrapper | Launching crafty with [$args]"
+    exec sudo -u crafty bash -c "source ./.venv/bin/activate && exec python3 main.py $args"
+else
+    # Activate our prepared venv
+    echo "Wrapper | Non-root host detected, using normal exec"
+    . ./.venv/bin/activate
+    # Use exec as our perms are already correct
+    # This is likely if using Kubernetes/OpenShift etc
+    exec python3 main.py $@
+fi
