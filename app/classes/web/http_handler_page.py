92579280a2f13c2d2f6c514819e2d3376d8eba44
Andrew <mcdeweykp@gmail.com>
2022-04-12 19:38:18 -0400

    Appease the linter

--- app/classes/web/http_handler_page.py
+++ app/classes/web/http_handler_page.py
@@ -1,7 +1,5 @@
 import logging
 import requests
-
-from app.classes.shared.helpers import helper
 from app.classes.web.base_handler import BaseHandler
 
 logger = logging.getLogger(__name__)
@@ -16,13 +14,15 @@ class HTTPHandlerPage(BaseHandler):
         url_list = url.split("/")
         if url_list[0] != "":
             primary_url = url_list[0] + ":" + str(port) + "/"
-            backup_url = url_list[0] + ":" + str(helper.get_setting("https_port")) + "/"
+            backup_url = (
+                url_list[0] + ":" + str(self.helper.get_setting("https_port")) + "/"
+            )
             for i in range(len(url_list) - 1):
                 primary_url += url_list[i + 1]
                 backup_url += url_list[i + 1]
         else:
             primary_url = url + str(port)
-            backup_url = url + str(helper.get_setting("https_port"))
+            backup_url = url + str(self.helper.get_setting("https_port"))
 
         try:
             resp = requests.get(primary_url)
