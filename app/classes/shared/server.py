4e84eee5a567235cc2454c76230350311e0a7275
luukas <lukasdoesdev@gmail.com>
2022-05-25 16:15:51 +0300

    Rename server stats variables to server_stats

--- app/classes/shared/server.py
+++ app/classes/shared/server.py
@@ -1444,12 +1444,12 @@ class Server:
 
     def record_server_stats(self):
 
-        server = self.get_servers_stats()
-        HelperServerStats.insert_server_stats(server)
+        server_stats = self.get_servers_stats()
+        HelperServerStats.insert_server_stats(server_stats)
 
         # delete old data
         max_age = self.helper.get_setting("history_max_age")
         now = datetime.datetime.now()
         minimum_to_exist = now - datetime.timedelta(days=max_age)
 
-        HelperServerStats.remove_old_stats(server.get("id", 0), minimum_to_exist)
+        HelperServerStats.remove_old_stats(server_stats.get("id", 0), minimum_to_exist)
