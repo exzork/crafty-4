7885b2c8f70bb7245bc5f947880264441cf0e528
luukas <lukasdoesdev@gmail.com>
2022-05-28 21:11:09 +0300

    Fix null writer

--- app/classes/shared/null_writer.py
+++ app/classes/shared/null_writer.py
@@ -6,5 +6,7 @@ logger = logging.getLogger(__name__)
 
 class NullWriter:
     def write(self, data):
-        if os.environ["CRAFTY_LOG_NULLWRITER"] == "true":
+        if os.environ.get("CRAFTY_LOG_NULLWRITER", "false") == "true":
             logger.debug(data)
+        if os.environ.get("CRAFTY_PRINT_NULLWRITER", "false") == "true":
+            print(data)
