fdc88451b97c935fe61deab97414d8d423483068
Andrew <mcdeweykp@gmail.com>
2022-06-01 15:11:37 -0400

    Add exception for permissions in helpers check

--- app/classes/shared/helpers.py
+++ app/classes/shared/helpers.py
@@ -690,6 +690,9 @@ class Helpers:
         # directory already exists - non-blocking error
         except FileExistsError:
             pass
+        except PermissionError as e:
+            logger.critical(f"Check generated exception due to permssion error: {e}")
+            pass
 
     def create_self_signed_cert(self, cert_dir=None):
 
